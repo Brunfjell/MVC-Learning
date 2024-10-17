df549e1c52f92804abd699b7ee39683aa8ca11f5
Brunfjell <luistroyparaiso@gmail.com>
2024-10-18 01:47:35 +0800

    Add project files.

---
+++ Brunfjell/Data/BrunfjellContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Brunfjell.Models;
+
+namespace Brunfjell.Data
+{
+    public class BrunfjellContext : DbContext
+    {
+        public BrunfjellContext (DbContextOptions<BrunfjellContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brunfjell.Models.Gallery> Gallery { get; set; } = default!;
+    }
+}
---
+++ Brunfjell/Models/Gallery.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Brunfjell.Models
+{
+    public class Gallery
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
