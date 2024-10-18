1700918ee2a6ee189f6f2f640815664dd8aea47e
Brunfjell <luistroyparaiso@gmail.com>
2024-10-19 01:33:21 +0800

    Final MVC learning

--- Brunfjell/Models/Gallery.cs
+++ Brunfjell/Models/Gallery.cs
@@ -17,15 +17,15 @@ namespace Brunfjell.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
-        public decimal Price { get; set; }
-
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         [Required]
         [StringLength(30)]
         public string? Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         [StringLength(5)]
